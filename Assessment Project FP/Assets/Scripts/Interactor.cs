a6a6f2f435b21606c02273421bc453439c80faa6
FunnyUsername01 <144666876+FunnyUsername01@users.noreply.github.com>
2023-10-12 14:47:41 +0200

    Added interactivity system

---
+++ Assessment Project FP/Assets/Scripts/Interactor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface Interactable
+{
+    public void Interact();
+}
+public class Interactor : MonoBehaviour
+{
+    public Transform interactorSource;
+    public float interactRange;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray r = new Ray (interactorSource.position, interactorSource.forward);
+            if(Physics.Raycast(r, out RaycastHit hitInfo, interactRange))
+            {
+                if(hitInfo.collider.gameObject.TryGetComponent(out Interactable interactObj))
+                {
+                    interactObj.Interact();
+                }
+            }
+        }
+    }
+}
